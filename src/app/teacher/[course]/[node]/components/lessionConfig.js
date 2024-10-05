cd3518d6f5ce251687cca7e82606c73ef6161c29
unknown <mrifat46@gmail.com>
2024-10-05 19:01:48 +0600

    web address api

--- src/app/teacher/[course]/[node]/components/lessionConfig.js
+++ src/app/teacher/[course]/[node]/components/lessionConfig.js
@@ -10,7 +10,7 @@ const LessionConfig = ({chapter, params, type}) => {
   const router = useRouter()
 
   const addData = async () => {
-    const res = await fetch("http://localhost:3000/api/addDataChapter", {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/addDataChapter`, {
     method: "POST",
     headers: {
       'Content-Type': 'application/json'
